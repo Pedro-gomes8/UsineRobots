d3e98579458af43c2cdc7c7eb036a50fe24bdc75
oretmj <oreste.mosciatti@uc.cl>
2025-03-11 18:20:20 +0100

    feat: input arm almost done, missing Cisco Router and dropping object notification

--- arm_node/src/py_armnode_pkg/py_armnode/armnode.py
+++ arm_node/src/py_armnode_pkg/py_armnode/armnode.py
@@ -7,7 +7,15 @@ from py_armnode_pkg.srv import TurtleBotArrived
 import rclpy
 from rclpy.node import Node
 
-from pyniryo import NiryoRobot, JointsPosition
+import os
+import sys
+
+import time
+from pyniryo import JointsPosition, PoseObject, ObjectColor
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', '..', 'camera', 'cameraNiryo', 'src')))
+
+from RobotTriage import RobotTriage
 
 from turtleBotClass import TurtleBot
 
@@ -20,24 +28,34 @@ class ArmNed2Node(Node):
         # init parameters
         self.declare_parameter('x_pos', 0.1)
         self.declare_parameter('y_pos', 0.1)
-        self.declare_parameter('x_direction', 1)
+        self.declare_parameter('x_direction', -1)
+        self.declare_parameter('objectsOffset', 0.004)
         
         # init variables
         # turtleBots info
-        self.trutleBotRight = TurtleBot()
-        self.trutleBotLeft = TurtleBot()
+        self.turtleBotRight = TurtleBot()
+        self.turtleBotLeft = TurtleBot()
         
         # setup Niryo Arm (ip should be given)
-        self.robot = NiryoRobot('192.168.242.126')
-        self.robot.calibrate_auto()
-        self.robot.update_tool()
-        self.robot.move(JointsPosition(0.2, -0.3, 0.1, 0.0, 0.5, -0.8))
+        observationPose = PoseObject(0.219, -0.008, 0.2886, 3.025, 1.346, 2.976)
+        self.robot = RobotTriage('10.10.10.10', 'SalleTpSetup', observationPose, 1)
         
         # setup services
         self.srv1 = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
         self.srv2 = self.create_service(TurtleBotArrived, 'srv/TurtleBotArrived', self.turtleBotArrived)
-        
         self.get_logger().info("Service is ready")
+        
+        # setup clients
+        #self.cli = self.create_client(NotifyObjectMovement, 'srv/NotifyObjectMovement')
+        #while not self.cli.wait_for_service(timeout_sec=1.0):
+        #    self.get_logger().info('service not available, waiting again...')
+        #self.get_logger().info("Client is ready")
+        #self.req = NotifyObjectMovement.Request()
+
+        self.robot.observe()
+        
+        timer_period = 10  # seconds
+        self.timer = self.create_timer(timer_period, self.checkForObjects)
 
     def add_two_ints_callback(self, request, response):
         print("callback")
@@ -67,21 +85,146 @@ class ArmNed2Node(Node):
         self.get_logger().info('Incoming request\ny_pos_Arm: %f x_direction: %d x_pos_Arm: %f color: %s ' % (self.get_parameter('y_pos').get_parameter_value().double_value, self.get_parameter('x_direction').get_parameter_value().integer_value, self.get_parameter('x_pos').get_parameter_value().double_value, request.data.color))
         
         if isRight:
-            self.trutleBotRight.turtleBotArrived(request.data.x_pos, request.data.y_pos, request.data.color, request.data.id)
+            self.turtleBotRight.turtleBotArrived(request.data.x_pos, request.data.y_pos, request.data.color, request.data.id)
             self.get_logger().info('Setting Right turtleBot')
         else:
-            self.trutleBotLeft.turtleBotArrived(request.data.x_pos, request.data.y_pos, request.data.color, request.data.id)
+            self.turtleBotLeft.turtleBotArrived(request.data.x_pos, request.data.y_pos, request.data.color, request.data.id)
             self.get_logger().info('Setting Left turtleBot')
         return response
+        
+    def notifyObjectMovement(self, turtle_id, obj_diff, object_color):
+        self.get_logger().info('Incoming request\nturtle_id: %d obj_diff: %d object_color: %s' % (turtle_id, obj_diff, object_color))
+
+        #self.req.turtle_id = turtle_id
+        #self.req.obj_diff = obj_diff
+        #self.req.object_color = object_color
+
+        #future = self.cli.call_async(self.req)
+        #rclpy.spin_until_future_complete(self, self.future)
+
+        #if self.future.result() is not None:
+         #   self.get_logger().info('Result of object movement: for turtle_id: %d is %d' % (turtle_id, self.future.result().ack))
+            # ack = 1 means the turtleBot leaves the arm to switch side
+          #  if (self.future.result().ack == 1):
+           #     if (self.turtleBotRight.id == turtle_id):
+            #        self.turtleBotRight.turtleBotLeft()
+             #   else:
+              #      self.turtleBotLeft.turtleBotLeft()
+
+        #return self.future.result()
+        
+    def checkForObjects(self):
+        self.get_logger().info('Checkin for objects')
+        availablesColors = []
+        turtleWithoutColor = False
+        if self.turtleBotRight.isThere:
+            if self.turtleBotRight.objectsColors == "":
+                turtleWithoutColor = True
+            else:
+                availablesColors.append(self.turtleBotRight.objectsColors)
+        if self.turtleBotLeft.isThere:
+            if self.turtleBotLeft.objectsColors == "":
+                turtleWithoutColor = True
+            else:
+                availablesColors.append(self.turtleBotLeft.objectsColors)
+
+        objectColor = ObjectColor
+
+        armPickedObject = False
+
+        if turtleWithoutColor:
+            objectColor = ObjectColor.ANY
+            armPickedObject, objectColor = self.robot.pickObject(self.get_parameter('objectsOffset').get_parameter_value().double_value, color = objectColor)
+
+        else: 
+            for color in availablesColors:
+                if (color == "red"):
+                    objectColor = ObjectColor.RED
+                elif (color == "green"):
+                    objectColor = ObjectColor.GREEN
+                elif (color == "blue"):
+                    objectColor = ObjectColor.BLUE
+
+                armPickedObject, objectColor = self.robot.pickObject(self.get_parameter('objectsOffset').get_parameter_value().double_value, color = objectColor)
+                if armPickedObject:
+                    break
+            
+        if armPickedObject:
+            #self.robot.observe()
+            color = ""
+            if objectColor == ObjectColor.RED:
+                color = "red"
+            elif objectColor == ObjectColor.GREEN:
+                color = "green"	
+            elif objectColor == ObjectColor.BLUE:
+                color = "blue"
+                
+            if color in availablesColors:
+                print("the color was in the list")
+                if self.turtleBotRight.isThere and self.turtleBotRight.objectsColors == color:
+                    pose = self.getPoseOverTurtle(self.turtleBotRight.id)
+                    self.robot.placeObject(pose)
+                    self.notifyObjectMovement(self.turtleBotRight.id, 1, color)
+                else:
+                    pose = self.getPoseOverTurtle(self.turtleBotLeft.id)
+                    self.robot.placeObject(pose)
+                    self.notifyObjectMovement(self.turtleBotLeft.id, 1, color)
+                    
+            else: 
+                print("turtles with no colors")
+                if self.turtleBotRight.isThere and self.turtleBotRight.objectsColors == "":
+                    pose = self.getPoseOverTurtle(self.turtleBotRight.id)
+                    self.robot.placeObject(pose)
+                    self.notifyObjectMovement(self.turtleBotRight.id, 1, color)
+                    self.turtleBotRight.setObjectColor(color)
+                else:
+                    pose = self.getPoseOverTurtle(self.turtleBotLeft.id)
+                    self.robot.placeObject(pose)
+                    self.notifyObjectMovement(self.turtleBotLeft.id, 1, color)
+                    self.turtleBotLeft.setObjectColor(color)
+
+        self.robot.observe()
+        
+    # calculate a pose to place the object over the turtleBot coordinates based on the x_pos and y_pos as 0 0
+    def getPoseOverTurtle(self, turtle_id):
+        # position of the arm and direction in wich it is facing (+x or -x)
+        x_pos = self.get_parameter('x_pos').get_parameter_value().double_value
+        y_pos = self.get_parameter('y_pos').get_parameter_value().double_value
+        x_direction = self.get_parameter('x_direction').get_parameter_value().integer_value
+
+        if turtle_id == self.turtleBotRight.id:
+            turtleBotNextObjectCoord = self.turtleBotRight.getCoordonatesNextObject()
+            pose_x = turtleBotNextObjectCoord[0] - x_pos
+            pose_y = turtleBotNextObjectCoord[1] - y_pos
+            if (x_direction < 0):
+                pose_y = -pose_y
+                pose_x = -pose_x
+            return PoseObject(round(pose_x, 4), round(pose_y,4), 0.1933, 0.102, 1.465, -1.282)
+
+        else:
+            turtleBotNextObjectCoord = self.turtleBotLeft.getCoordonatesNextObject()
+            pose_x = turtleBotNextObjectCoord[0] - x_pos
+            pose_y = turtleBotNextObjectCoord[1] - y_pos
+            if (x_direction < 0):
+                pose_y = -pose_y
+                pose_x = -pose_x
+        return PoseObject(round(pose_x, 4), round(pose_y,4), 0.1933, 1.993, 1.489, -2.579)
+
 
 def main(args=None):
     rclpy.init(args=args)
 
     minimal_service = ArmNed2Node()
 
+    #lastCheck = 0
+    #deltaTime = 10
+    
     while rclpy.ok():
     	rclpy.spin_once(minimal_service)
-
+    	#if time.time() - lastCheck > deltaTime:
+    	 #   minimal_service.checkForObjects()
+    	  #  lastCheck = time.time()
+    	    
     print("Shutting Down")
 
     rclpy.shutdown()
--- arm_node/src/py_armnode_pkg/py_armnode/turtleBotClass.py
+++ arm_node/src/py_armnode_pkg/py_armnode/turtleBotClass.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
+import math
+
 class TurtleBot:
-    def _init_(self):
+    def __init__(self, maxObjects = 5, radiusObjectPlacement = 0.06):
         self.isThere = False
         self.x = 0
         self.y = 0
         self.objectsColors = ""
         self.id = ""
+        self.numbOfObjects = 0
+        self.maxObjects = maxObjects
+        self.radiusObjectPlacement = radiusObjectPlacement
 
     def turtleBotArrived(self, x, y, objectsColors, id):
         self.isThere = True
@@ -14,9 +19,10 @@ class TurtleBot:
         self.y = y
         self.objectsColors = objectsColors
         self.id = id
+        self.numbOfObjects = 0
 
 
-    def addObjectColor(self, objectColor):
+    def setObjectColor(self, objectColor):
         self.objectsColors = objectColor
 
     def turtleBotLeft(self):
@@ -25,3 +31,11 @@ class TurtleBot:
         self.y = 0
         self.objectsColors = ""
         self.id = ""
+        self.numbOfObjects = 0
+        
+    def getCoordonatesNextObject(self):
+        if self.numbOfObjects < self.maxObjects:
+            self.numbOfObjects += 1
+            return [self.x + self.radiusObjectPlacement * math.cos(2*math.pi*self.numbOfObjects/self.maxObjects), self.y + self.radiusObjectPlacement * math.sin(2*math.pi*self.numbOfObjects/self.maxObjects)]
+        else:
+            return [self.x, self.y]
