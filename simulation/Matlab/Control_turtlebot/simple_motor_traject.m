abe2aa3aaa380c0ea57c54b49dfe153afc0f98b6
Gabriel Fontenele <gabrielfontenele.ufc@gmail.com>
2025-03-14 08:36:35 +0100

    Upload Matlab Simulation

--- simulation/Matlab/Control_turtlebot/simple_motor_traject.m
+++ simulation/Matlab/Control_turtlebot/simple_motor_traject.m
@@ -2,22 +2,31 @@
 % Motor Parameters
 Km = 50/1000 ; % 50 m*s*V'
 Tau = 0.025; %sec
-l = 90/1000; % meters
-L = 115/1000; % meters
+R = 0.17; % [m]
+V_max = 0.7; % m/s
+W_max = pi; % rad/s
 
-param = [Km Tau l L];
+param = [Km Tau R];
 
 K_ff = 0.02;
-    
-Tf = 20;
 
+u_sat = 0.5*V_max;
+
+pre_gain_v = 1/0.8;
+pre_gain_w = 1/0.9;
+
+% ===========================
 px0 = 0;
 py0 = 0;
 theta0 = 0;
 
-x_ref = 1;
-y_ref = 1;
+x_ref = 10;
+y_ref = 15;
 
+
+% ==========================
+
+Tf = 100;
 sim('robot.slx',Tf);
 
 % 
